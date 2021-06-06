a0b813638de01c5116cd0cf4f368c815d017f37d
sandaramet <aram@gratun.am>
2021-06-06 23:00:55 +0400

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,41 @@
 $(document).ready(() => {
-  
-  animateInScroll();
-  animateInScroll(
-    [
+  (() => {
+    const an = [
+      "./assets/img/SVG/spoon/phase-1.svg",
+      "./assets/img/SVG/spoon/phase-2.svg",
+      "./assets/img/SVG/spoon/phase-3.svg",
+      "./assets/img/SVG/spoon/phase-4.svg",
+      "./assets/img/SVG/spoon/phase-3.svg",
+      "./assets/img/SVG/spoon/phase-2.svg",
+      "./assets/img/SVG/spoon/phase-1.svg",
+    ];
+    let i = 0;
+    setInterval(() => {
+      if (i > an.length) i = 0;
+      console.log(an[i]);
+      $("#myimg").attr("src", an[i]);
+      i++;
+    }, 300);
+  })();
+
+  (() => {
+    const an = [
       "./assets/img/SVG/flying bee/1.svg",
       "./assets/img/SVG/flying bee/2.svg",
       "./assets/img/SVG/flying bee/3.svg",
       "./assets/img/SVG/flying bee/2.svg",
-      "./assets/img/SVG/flying bee/1.svg",
-    ],
-    (className = ".flying-bee img")
-  );
+
+    ];
+    let i = 0;
+    setInterval(() => {
+      if (i > an.length) i = 0;
+      console.log(an[i]);
+      // $(".flying-bee img").attr("src", an[i]);
+      i++;
+    }, 100);
+  })();
+
+
   function animateInScroll(
     arr = [
       "./assets/img/SVG/spoon/phase-1.svg",
@@ -51,21 +76,24 @@ $(document).ready(() => {
       .addTo(controller);
   }
 
+  $(".graphic img").parent().addClass("viewer")
   $(".place-name").parent().parent().css("box-shadow", "none");
-  $("img[data-count]").each(function(){
-    console.log($(this).data('count'))
-    $(this).css('transform',`scale(calc(1.${$(this).data('count')} - 0.5))`)
+  $("img[data-count]").each(function () {
+    console.log($(this).data("count"));
+    $(this).css("transform", `scale(calc(1.${$(this).data("count")} - 0.5))`);
+  });
+  // View an image.
+  for (let i = 0; i < 30; i++) {
+    const viewer = new Viewer(document.querySelectorAll(".viewer")[i], {
+      // inline: true,
+      navbar:false,
+      toolbar:false,
+      title:false,
+      viewed() {
+        // viewer.zoomTo(1);
+      },
+    });
 
-  }) 
-
-  // $($(".place-name").parent().find("img")).css("border-radius", "8px");
-
-  //   var controller = new ScrollMagic.Controller();
-
-  //   // build scene
-  //   var scene = new ScrollMagic.Scene({triggerElement: "#trigger"})
-  // 				  // trigger a velocity opaticy animation
-  // 				  .setVelocity("section", {opacity: 1}, {duration: 400})
-  // 				  .addIndicators() // add indicators (requires plugin)
-  // 				  .addTo(controller);
+  }
+ 
 });
