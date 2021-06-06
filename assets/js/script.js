054bd0142cf6e4e6c6af1cda1cf5b4cb94fdf8a3
sandaramet <aram@gratun.am>
2021-06-07 03:12:44 +0400

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,18 +80,20 @@ $(document).ready(() => {
     $(this).css("transform", `scale(calc(0.${$(this).data("count")} * 1.5 ))`);
   });
   // View an image.
+  console.log(window.parent);
+
   for (let i = 0; i < 10; i++) {
-    new Viewer(document.querySelectorAll(".viewer")[i], {
+   let viewer =  new Viewer(document.querySelectorAll(".viewer")[i], {
       // inline: true,
-      container:document.querySelectorAll(".viewer")[i],
       navbar:false,
       toolbar:false,
       title:false,
       viewed() {
         // viewer.zoomTo(1);
       },
+      moveTo(){
+        console.log("this")
+      }
     });
-
   }
-  // console.log(window.parent);
 });
