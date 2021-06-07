5617071d99285c1f8a20cf96ce926e5575f9fe9c
sandaramet <aram@gratun.am>
2021-06-07 06:30:33 +0400

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,23 +17,41 @@ $(document).ready(() => {
     }, 200);
   })();
 
-  
-
-  
-  $(".graphic img").parent().addClass("viewer")
+  $(".graphic img").parent().addClass("viewer");
   $(".place-name").parent().parent().css("box-shadow", "none");
   $("img[data-count]").each(function () {
     $(this).css("transform", `scale(calc(0.${$(this).data("count")} * 1.5 ))`);
   });
   // View an image.
   for (let i = 0; i < 10; i++) {
-   let viewer =  new Viewer(document.querySelectorAll(".viewer")[i], {
+    let viewer = new Viewer(document.querySelectorAll(".viewer")[i], {
       // inline: true,
-      navbar:false,
-      toolbar:false,
-      title:false,
-   
+      navbar: false,
+      toolbar: false,
+      title: false,
     });
   }
   // console.log(window.parent);
+  const effect = [
+    "fade-up",
+    "fade-down",
+    "fade-right",
+    "fade-left",
+    "zoom-in",
+    "zoom-out",
+    "slide-up",
+    "flip-up",
+    "flip-down",
+    "flip-right",
+    "flip-left",
+  ];
+
+  $("section").each(function () {
+    console.log($(this));
+
+    $(this).attr("data-aos", effect[Math.floor(Math.random() * effect.length + 1)]);
+  });
+  AOS.refresh();
+
+  AOS.init();
 });
